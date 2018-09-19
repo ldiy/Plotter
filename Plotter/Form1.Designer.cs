aeb2bb5a5d3666ef0e3200a82225045a7c44330b
Lorenz Clijnen <lorenz_reclame@outlook.be>
2018-09-19 21:33:03 +0200

    d

---
+++ Plotter/Form1.Designer.cs
@@ -0,0 +1,282 @@
+﻿namespace Plotter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.print = new System.Windows.Forms.Button();
+            this.SelectImage = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.calculate = new System.Windows.Forms.Button();
+            this.Connect = new System.Windows.Forms.Button();
+            this.result = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Selected_Image = new System.Windows.Forms.ComboBox();
+            this.Selected_image_x = new System.Windows.Forms.NumericUpDown();
+            this.Selected_image_y = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.Selected_image_remove = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.result)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Selected_image_x)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Selected_image_y)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(600, 450);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
+            // 
+            // print
+            // 
+            this.print.Location = new System.Drawing.Point(615, 434);
+            this.print.Name = "print";
+            this.print.Size = new System.Drawing.Size(300, 29);
+            this.print.TabIndex = 1;
+            this.print.Text = "Print";
+            this.print.UseVisualStyleBackColor = true;
+            this.print.Click += new System.EventHandler(this.print_Click);
+            // 
+            // SelectImage
+            // 
+            this.SelectImage.Location = new System.Drawing.Point(621, 10);
+            this.SelectImage.Name = "SelectImage";
+            this.SelectImage.Size = new System.Drawing.Size(102, 23);
+            this.SelectImage.TabIndex = 2;
+            this.SelectImage.Text = "Select Image";
+            this.SelectImage.UseVisualStyleBackColor = true;
+            this.SelectImage.Click += new System.EventHandler(this.SelectImage_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(86, 187);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(211, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Drag and Drop Image or click Select Image";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // calculate
+            // 
+            this.calculate.Location = new System.Drawing.Point(618, 177);
+            this.calculate.Name = "calculate";
+            this.calculate.Size = new System.Drawing.Size(174, 23);
+            this.calculate.TabIndex = 4;
+            this.calculate.Text = "Calclulate Gcode";
+            this.calculate.UseVisualStyleBackColor = true;
+            this.calculate.Click += new System.EventHandler(this.calculate_Click);
+            // 
+            // Connect
+            // 
+            this.Connect.Location = new System.Drawing.Point(621, 134);
+            this.Connect.Name = "Connect";
+            this.Connect.Size = new System.Drawing.Size(75, 23);
+            this.Connect.TabIndex = 5;
+            this.Connect.Text = "Connect";
+            this.Connect.UseVisualStyleBackColor = true;
+            this.Connect.Click += new System.EventHandler(this.Connect_Click);
+            // 
+            // result
+            // 
+            this.result.BackColor = System.Drawing.Color.White;
+            this.result.Location = new System.Drawing.Point(615, 203);
+            this.result.Name = "result";
+            this.result.Size = new System.Drawing.Size(300, 225);
+            this.result.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.result.TabIndex = 6;
+            this.result.TabStop = false;
+            this.result.Paint += new System.Windows.Forms.PaintEventHandler(this.result_Paint);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(798, 182);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Result:";
+            // 
+            // Selected_Image
+            // 
+            this.Selected_Image.FormattingEnabled = true;
+            this.Selected_Image.Location = new System.Drawing.Point(621, 40);
+            this.Selected_Image.Name = "Selected_Image";
+            this.Selected_Image.Size = new System.Drawing.Size(133, 21);
+            this.Selected_Image.TabIndex = 8;
+            this.Selected_Image.Text = "Select a image";
+            // 
+            // Selected_image_x
+            // 
+            this.Selected_image_x.Location = new System.Drawing.Point(783, 41);
+            this.Selected_image_x.Name = "Selected_image_x";
+            this.Selected_image_x.Size = new System.Drawing.Size(55, 20);
+            this.Selected_image_x.TabIndex = 9;
+            this.Selected_image_x.ValueChanged += new System.EventHandler(this.Selected_image_x_ValueChanged);
+            // 
+            // Selected_image_y
+            // 
+            this.Selected_image_y.Location = new System.Drawing.Point(864, 41);
+            this.Selected_image_y.Name = "Selected_image_y";
+            this.Selected_image_y.Size = new System.Drawing.Size(51, 20);
+            this.Selected_image_y.TabIndex = 10;
+            this.Selected_image_y.ValueChanged += new System.EventHandler(this.Selected_image_y_ValueChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(760, 43);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(17, 13);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "X:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(844, 43);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(17, 13);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Y:";
+            // 
+            // Selected_image_remove
+            // 
+            this.Selected_image_remove.Location = new System.Drawing.Point(621, 68);
+            this.Selected_image_remove.Name = "Selected_image_remove";
+            this.Selected_image_remove.Size = new System.Drawing.Size(75, 23);
+            this.Selected_image_remove.TabIndex = 13;
+            this.Selected_image_remove.Text = "Remove";
+            this.Selected_image_remove.UseVisualStyleBackColor = true;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(783, 66);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(55, 20);
+            this.numericUpDown1.TabIndex = 14;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(747, 68);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(30, 13);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "Size:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(844, 68);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(20, 17);
+            this.label6.TabIndex = 16;
+            this.label6.Text = "%";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(923, 473);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.Selected_image_remove);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.Selected_image_y);
+            this.Controls.Add(this.Selected_image_x);
+            this.Controls.Add(this.Selected_Image);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.result);
+            this.Controls.Add(this.Connect);
+            this.Controls.Add(this.calculate);
+            this.Controls.Add(this.SelectImage);
+            this.Controls.Add(this.print);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Plotter";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.DragDrop += new System.Windows.Forms.DragEventHandler(this.Form1_DragDrop);
+            this.DragEnter += new System.Windows.Forms.DragEventHandler(this.Form1_DragEnter);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.result)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Selected_image_x)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Selected_image_y)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button print;
+        private System.Windows.Forms.Button SelectImage;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button calculate;
+        private System.Windows.Forms.Button Connect;
+        private System.Windows.Forms.PictureBox result;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox Selected_Image;
+        private System.Windows.Forms.NumericUpDown Selected_image_x;
+        private System.Windows.Forms.NumericUpDown Selected_image_y;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button Selected_image_remove;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ Plotter/Form1.cs
@@ -0,0 +1,368 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.IO.Ports;
+using System.Windows.Forms;
+
+
+namespace Plotter
+{
+    public partial class Form1 : Form
+    {
+        //---------config----------//
+        String pen_down = "G0 Z0";
+        String pen_up = "G0 Z100";    
+        string poort = "COM3";      
+        int baud = 112500;
+        //------------------------//
+
+        private SerialPort myport;
+        Bitmap image;
+        Bitmap result_image;
+
+        Bitmap[] images = new Bitmap[30];
+        int images_index = 0;
+        int start_x = 0;
+        int start_y = 0;
+
+        bool mouse_status = false;
+        Point prev_point = Point.Empty;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        void find_begin() // find first pixel != white
+        {
+            
+            for (int y = 0; y < image.Height; y++)
+            {
+
+                for (int x = 0; x < image.Width; x++)
+                {
+
+                    if (image.GetPixel(x, y).Name != "ffffffff")
+                    {
+                        start_x = x;
+                        start_y = y;
+                        return;
+                    }
+                }
+            }
+        }
+        bool check_around(int x, int y) // look for arounding pixels that are white
+        {
+
+            if (image.GetPixel(x + 1, y + 1).Name == "ffffffff")
+            {
+                return true;
+            }
+            if (image.GetPixel(x + 1, y).Name == "ffffffff")
+            {
+                return true;
+            }
+            if (image.GetPixel(x + 1, y - 1).Name == "ffffffff")
+            {
+                return true;
+            }
+            if (image.GetPixel(x - 1, y - 1).Name == "ffffffff")
+            {
+                return true;
+            }
+            if (image.GetPixel(x - 1, y + 1).Name == "ffffffff")
+            {
+                return true;
+            }
+            if (image.GetPixel(x, y + 1).Name == "ffffffff")
+            {
+                return true;
+            }
+            if (image.GetPixel(x,y - 1).Name == "ffffffff")
+            {
+                return true;
+            }
+            if (image.GetPixel(x - 1, y).Name == "ffffffff")
+            {
+                return true;
+            }
+
+            return false;
+        }
+        private void Form1_DragDrop(object sender, DragEventArgs e)
+        {
+            string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+
+            //pictureBox1.Image = Image.FromFile(files[0]);
+            Bitmap temp = new Bitmap(Image.FromFile(files[0]));
+            Selected_Image.Items.Add( files[0]);
+            images[images_index] = temp;
+            label1.Text = "";
+            calculate.Enabled = true;
+        }
+
+        private void Form1_DragEnter(object sender, DragEventArgs e)
+        {
+            e.Effect = DragDropEffects.Move;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            AllowDrop = true;
+            print.Enabled = false;
+            calculate.Enabled = true;
+            image = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+        }
+
+        private void print_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void SelectImage_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                pictureBox1.Image = Image.FromFile(openFileDialog1.FileName);
+                label1.Text = "";
+                calculate.Enabled = true;
+            }
+        }
+
+        private void calculate_Click(object sender, EventArgs e)
+        {
+
+            if (pictureBox1.Image == null)
+            {
+                return;
+            }
+            image = new Bitmap(pictureBox1.Image);
+            print.Enabled = false;
+            StreamWriter sw = new StreamWriter("gcode.txt"); //open file to write gcode
+            int[,] writed = new int[image.Width, image.Height]; //array with points already in gcode
+            result_image = new Bitmap(image.Width, image.Height);
+            
+
+            find_begin(); //find first pixel != white
+             int current_x = start_x;
+             int current_y = start_y;
+             int prev_direction = 0;
+
+             sw.WriteLine(pen_down); //write pen down in gcode
+
+             for (; ; ) { //loop infinity
+                 int direction = 8; //set default direction 
+
+                //check for arounded pixel that isn't white and set direction
+                 if (image.GetPixel(current_x + 1, current_y + 1).Name != "ffffffff" && writed[current_x+1, current_y+1] != 1 && check_around(current_x+1,current_y+1))
+                 {
+                     direction = 2;
+                 }
+                 if (image.GetPixel(current_x + 1, current_y).Name  != "ffffffff" && writed[current_x+1, current_y] != 1 && check_around(current_x+1, current_y))
+                 {
+                     direction = 0;
+                 }
+                 if (image.GetPixel(current_x + 1, current_y - 1).Name  != "ffffffff" && writed[current_x+1, current_y-1] != 1 && check_around(current_x+1, current_y-1))
+                 {
+                     direction = 1;
+                 }
+                 if (image.GetPixel(current_x - 1, current_y - 1).Name != "ffffffff" && writed[current_x-1, current_y-1] != 1 && check_around(current_x-1, current_y-1))
+                 {
+                     direction = 3;
+                 }
+                 if (image.GetPixel(current_x - 1, current_y + 1).Name != "ffffffff" && writed[current_x-1, current_y+1] != 1 && check_around(current_x-1, current_y+1))
+                 {
+                     direction = 4;
+                 }
+                 if (image.GetPixel(current_x, current_y + 1).Name != "ffffffff" && writed[current_x, current_y+1] != 1 && check_around(current_x, current_y+1))
+                 {
+                     direction = 5;
+                 }
+                 if (image.GetPixel(current_x, current_y - 1).Name != "ffffffff" && writed[current_x, current_y-1] != 1 && check_around(current_x, current_y-1))
+                 {
+                     direction = 6;
+                 }
+                 if (image.GetPixel(current_x - 1, current_y).Name != "ffffffff" && writed[current_x-1, current_y] != 1 && check_around(current_x-1, current_y))
+                 {
+                     direction = 7;
+                 }
+
+                 //if no arounded pixel is found
+                 if(direction == 8)
+                 {
+                     int ok = 0;
+                     for(int y=0; y < image.Height && ok ==0; y++) //loop through all pixels
+                     {
+                         for(int x =0; x<image.Width && ok == 0; x++)
+                         {
+                             if (image.GetPixel(x, y).Name != "ffffffff" && writed[x,y] != 1 && check_around(x,y)) //check for pixel != white that isn't already writed.
+                             {
+                                 current_x = x;
+                                 current_y = y;
+                                 sw.WriteLine(pen_up);// write pen up to gcode fiel
+                                 sw.WriteLine("G0 X" + current_x + " Y" + current_y + " F2400"); //move to coordinate of pixel != white
+                                 sw.WriteLine(pen_down); //write pen down to gcode file
+                                 ok = 1; //stop with looping
+                                 writed[x, y] = 1; //set founden pixel in array writed
+                              }
+                         }
+                     }
+                     if(ok == 0) //all pixels have been writed to gcode file
+                     {
+                         sw.WriteLine(pen_up); //write pen up
+                         //Close the file
+                         sw.Close();
+                        
+                     /*   for (int y = 0; y < image.Height && ok == 0; y++) //loop through all pixels
+                        {
+                            for (int x = 0; x < image.Width && ok == 0; x++)
+                            {
+                                if (writed[x,y] == 1) { 
+                                    result_image.SetPixel(x, y, Color.Black);
+                                    
+                                }
+                            }
+                        }*/
+                        result.Image = result_image;
+                        print.Enabled = true; // enable print button
+                         return; // break out of infinity loop
+                     }
+                     
+                 }
+                 if (direction != prev_direction) //check if direction changes
+                 {
+                     sw.WriteLine("G0 X" + current_x + " Y" + current_y + " F2400"); //move to current coordinate
+                 }
+
+                 switch (direction)
+                 {
+                     case 0:
+                         writed[current_x, current_y] = 1;
+                        result_image.SetPixel(current_x, current_y, Color.Black);
+                        current_x++;
+                         break;
+
+                     case 1:
+                         writed[current_x, current_y] = 1;
+                        result_image.SetPixel(current_x, current_y, Color.Black);
+                        current_x++;
+                         current_y--;
+                         break;
+
+                     case 2:
+                         writed[current_x, current_y] = 1;
+                        result_image.SetPixel(current_x, current_y, Color.Black);
+                        current_x++;
+                         current_y++;
+                         break;
+                     case 3:
+                         writed[current_x, current_y] = 1;
+                        result_image.SetPixel(current_x, current_y, Color.Black);
+                        current_x--;
+                         current_y--;
+                         break;
+                     case 4:
+                         writed[current_x, current_y] = 1;
+                        result_image.SetPixel(current_x, current_y, Color.Black);
+                        current_x--;
+                         current_y++;
+                         break;
+                     case 5:
+                         writed[current_x, current_y] = 1;
+                        result_image.SetPixel(current_x, current_y, Color.Black);
+                        current_y++;
+                         break;
+                     case 6:
+                         writed[current_x, current_y] = 1;
+                        result_image.SetPixel(current_x, current_y, Color.Black);
+                        current_y--;
+                         break;
+                     case 7:
+                         writed[current_x, current_y] = 1;
+                        result_image.SetPixel(current_x, current_y, Color.Black);
+                        current_x--;
+                         break;
+
+                     default:
+                         break;
+
+
+                 }
+         }
+
+      }
+
+        private void Connect_Click(object sender, EventArgs e)
+        {
+          int  ok = 1;
+
+            try
+            {
+                myport = new SerialPort();
+                myport.BaudRate = baud;
+                myport.PortName = poort;
+                //  if (myport.IsOpen) myport.Close();
+                myport.Open();
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("kan geen verbinding maken op " + poort + ".\nprobeer een andere poort.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                ok = 0;
+            }
+            if (ok == 1)
+            {
+                MessageBox.Show("verbinding gemaakt op " + poort, "verbinding gemaakt.", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+
+            }
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            ControlPaint.DrawBorder(e.Graphics, pictureBox1.ClientRectangle, Color.Black, ButtonBorderStyle.Solid);
+        }
+
+        private void result_Paint(object sender, PaintEventArgs e)
+        {
+            ControlPaint.DrawBorder(e.Graphics, result.ClientRectangle, Color.Black, ButtonBorderStyle.Solid);
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (mouse_status && prev_point != null)
+            {
+                if (pictureBox1.Image == null)
+                {
+                    Bitmap bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+                    pictureBox1.Image = bmp;
+                }
+
+                using (Graphics g = Graphics.FromImage(pictureBox1.Image))
+                {
+                    g.DrawLine(new Pen(Color.Black, 2), prev_point, e.Location);
+                }
+                pictureBox1.Invalidate();
+                prev_point = e.Location;
+            }
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            mouse_status = true;
+            prev_point = e.Location;
+        }
+
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            mouse_status = false;
+        }
+
+        private void Selected_image_x_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Selected_image_y_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
